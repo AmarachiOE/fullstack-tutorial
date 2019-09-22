6ced8b6fdbcf8c6adcba6894ec3b9a0ed296a812
AmarachiOE <ao.ejiawoko@gmail.com>
2019-09-21 21:55:50 -0700

    schema initialization/imports

---
+++ start/server/src/schema.js
@@ -0,0 +1,13 @@
+/*
+
+In src/schema.js, import  gql from Apollo Server and create a variable called typeDefs for your schema. Your schema will go inside the gql function (between the backticks in this portion: gql``). 
+
+Make sure to export typeDegs
+
+*/
+
+const { gql } = require('apollo-server');
+const typeDefs = gql``;
+
+
+module.exports = typeDefs;
