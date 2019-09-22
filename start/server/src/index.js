7daea52720a26303e49099c8294ab4ed1ab4026d
AmarachiOE <ao.ejiawoko@gmail.com>
2019-09-21 21:51:43 -0700

    added notes

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,4 +1,8 @@
+// import ApolloServer class from  apollo-server
 const { ApolloServer } = require('apollo-server');
+
+// import our schema from  src/schema.js
 const typeDefs = require('./schema');
 
+// Next, let's create a new instance of ApolloServer and pass our schema to the typeDefs property on the configuration object. (typdeDefs: typeDefs)
 const server = new ApolloServer({ typeDefs });
