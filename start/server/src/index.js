e5a7d33e33f073fe08886a03d7d438515b755943
AmarachiOE <ao.ejiawoko@gmail.com>
2019-09-25 13:55:31 -0700

    importing resolver map to server (index.js)

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,12 +1,13 @@
+// #################  IMPORTS
+
 // import ApolloServer class from  apollo-server
 const { ApolloServer } = require('apollo-server');
 
 // import our schema from  src/schema.js
 const typeDefs = require('./schema');
 
-// import createStore function to set up/create database, as well as our data sources: LaunchAPI and UserAPI (in const server)
+// import createStore function to set up/create database
 const { createStore } = require('./utils');
-
 
 // import Launch API data source that connects our REST API
 const LaunchAPI = require('./datasources/launch');
@@ -14,6 +15,10 @@ const LaunchAPI = require('./datasources/launch');
 // import User API data source that connects SQL database
 const LaunchAPI = require('./datasources/launch');
 
+// connect resolver map to Apollo Server
+const resolvers = require('./resolvers');
+
+// #################  SETTING UP SERVER
 
 const store = createStore();
 
